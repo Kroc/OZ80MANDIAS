836de2bcd5f408832abf5c985e6ceb83352194f4
Kroc Camen <kroc@camendesign.com>
2014-02-08 23:11:34 +0000

    Continual small improvements to the text parser...
    Expect slow, tentative progress. This stuff is quite hard work to
    manage.

--- test.oz8.asm
+++ test.oz8.asm
@@ -1,10 +1,10 @@
-;a file to test OZ80MANDIAS parsing and assembling
+//a file to test OZ80MANDIAS parsing and assembling
 
-;test each of the Z80 mnemonics and parameter styles
+//test each of the Z80 mnemonics and parameter styles
 
-;add with carry
- ;adc A, r|$n|(HL|IX+$n|IY+$n)
- ;adc HL, BC|DE|HL|SP
+//add with carry
+ //adc A, r|$n|(HL|IX+$n|IY+$n)
+ //adc HL, BC|DE|HL|SP
 adc a, b
 adc a, $FF
 adc a, (hl)
